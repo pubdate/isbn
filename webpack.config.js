c5f4fbfde31d5ce3697cec4fa20d590fb977be9a
Victor Taleb <hello@victortaleb.com>
2022-08-06 10:36:38 +0200

    init: config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: { configFile: 'tsconfig.build.json' }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    globalObject: 'this',
+    library: {
+      name: 'isbn',
+      type: 'umd',
+      export: 'default'
+    }
+  }
+}
